3df38cedc521c8ee73cad6cace60542badaf3023
RIDDHI <RIDDHI@DESKTOP-NGOTAL1>
2023-10-16 14:22:36 +0530

    Assignment 1 commit

---
+++ src/assignment_1/A.java
@@ -0,0 +1,11 @@
+package assignment_1;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello! This is class A");
+
+	}
+
+}
